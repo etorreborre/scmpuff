b46b5e521ba92dfc4cd5ce59668908fe1410a7c5
etorreborre <etorreborre@yahoo.com>
2022-11-03 23:14:27 +0100

    build scmpuff

--- pkgs/scmpuff/default.nix
+++ pkgs/scmpuff/default.nix
@@ -1,29 +1,39 @@
 # Replace "stdenv" with the namespace or name of your language's builder
-{ self, stdenv, autoPatchelfHook }:
-# Replace "stdenv.mkDerivation" with your language's builder
-stdenv.mkDerivation {
+{ self,
+  stdenv,
+  lib,
+  alsaLib,
+  fetchurl,
+  openssl,
+  pulseaudio,
+  zlib,
+  autoPatchelfHook,
+}:
+
+stdenv.mkDerivation rec {
   pname = "scmpuff";
   version = "0.5.0";
+  MACOSX_DEPLOYMENT_TARGET = true;
 
   sourceRoot = ".";
 
   src = fetchurl {
     url = "https://github.com/mroth/scmpuff/releases/download/v${version}/scmpuff_${version}_macOS_x64.tar.gz";
-    sha256 = "sha256-4CkijAlenhht8tyk3nBULaBPE0GBf6DVII699/RmmWI=";
+    sha256 = "sha256-PoJju37utsJroOQhbC07FMJN8XokpClGSpzsdbtTLAA=";
   };
 
 
   # Add buildtime dependencies (not required at runtime)
   # to nativeBuildInputs.
   nativeBuildInputs = [
-    autoPatchelfHook
+    #autoPatchelfHook
   ];
 
   dontConfigure = true;
   dontBuild = true;
 
   installPhase = ''
-    install -m755 -D scmpuff_${version}_macOS_x64 $out/bin/scmpuff
+    install -m755 -D scmpuff $out/bin/scmpuff
   '';
 
   meta = with lib; {
